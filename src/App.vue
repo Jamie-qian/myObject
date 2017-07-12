f8b64a0ea6daeb3e24120b86c51f47f6c338ea2b
xiaoqianqian <xiaoqianqian@papayamobile.cn>
2017-07-12 14:23:49 +0800

    main.js加注释

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,13 @@ export default {
 }
 </script>
 <style lang="scss" scope>
-    *{margin:0;padding:0;font-size:14px;}
+    *{ margin: 0; padding: 0;font-size: 14px;}
     #app{
-        width:100%;margin: 0 auto; 
-        .container{width:1000px;margin: 10px auto;
+        font-family: 'Avenir',Arial, Helvetica, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        width: 100%;margin: 0 auto; 
+        .container{ width:1000px; margin: 10px auto;
             .nav{
                 margin:45px 0 45px 0 ;
             }
