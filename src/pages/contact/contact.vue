844b05f7e617f6823ea1acaafc21aa44e895b045
xiaoqianqian <xiaoqianqian@papayamobile.cn>
2018-10-24 15:14:04 +0800

    add img

--- src/pages/contact/contact.vue
+++ src/pages/contact/contact.vue
@@ -2,10 +2,32 @@
     <div class="contact">
         <!-- 调用组件 -->
         <!-- <c-collapse></c-collapse> -->
-        <el-table :data="model.tableDate" border v-loading="loading">
-            <el-table-column prop="date" label="日期"></el-table-column>
+        <el-table :data="model.tableDate" border>
+            <el-table-column>
+                <template slot-scope="scope">
+                    <img src="../../static/img/logo.png" alt="" width="20px">
+                </template>
+            </el-table-column>
+            <el-table-column type="expand">
+                <template slot-scope="props">
+                    <el-form label-position="left" inline class="demo-table-expand">
+                        <el-form-item label="项目">
+                            <span>{{ props.row.project }}</span>
+                        </el-form-item>
+                    </el-form>
+                </template>
+            </el-table-column>
+            <el-table-column prop="start" label="START">
+            </el-table-column>
+            <el-table-column prop="end" label="END">
+            </el-table-column>
             <el-table-column prop="name" label="姓名"></el-table-column>
-            <el-table-column prop="address" label="地址"></el-table-column>
+            <el-table-column prop="address" label="地址">
+                <template slot-scope="scope">
+                    <el-tag type="info">{{scope.row.address}}</el-tag>
+                </template>
+            </el-table-column>
+            
         </el-table>
         <el-pagination 
             style="margin-top:20px;"
@@ -20,6 +42,7 @@
 </template>
 <script>
     import { mapState, mapGetters, mapActions } from 'vuex'
+    
 
     export default {
         name: 'contact',
--- src/vuex/modules/contact/contact.js
+++ src/vuex/modules/contact/contact.js
@@ -1,79 +1,98 @@
 const state = {
     tableDate:[
         {
+            start: '2017-10-01',
+            end: '2018-10-08',
+            name: 'Jamie',
+            address: '西安 成都 重庆',
+            project:'不停揣测你的心里可有我姓名',
+            
+        },
+        {
+            start: '2018-04-20',
+            end: '2018-04-22',
+            name: 'Jamie',
+            address: '上海',
+            project:'迪士尼外滩',
+            
+        },
+        {
+            start: '2018-04-30',
+            end: '2018-05-01',
+            name: 'Jamie XJ',
+            address: '天津',
+            project: '五月天演唱会 五大道 意式风情街',
+            
+        },
+        {
+            start:'',
+            end:'',
+            date: '2016-05-04',
+            name: 'Jamie JT',
+            address: '河北张家口',
+            project: '天漠音乐节',
+            
+        },
+        {
             date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
+            name: 'Jamie',
+            address: '山西太原',
+            project: ''
+        },
+        {
+            date: '2016-05-03',
+            name: 'Jamie HM',
+            address: '山西太原'
+        },
+        {
+            date: '2016-05-04',
+            name: 'Jamie',
+            address: '山东济南'
+        },
+        {
+            date: '2016-05-02',
+            name: 'Jamie',
+            address: '上海'
         },
         {
             date: '2016-05-03',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
+            address: '上海市普陀区'
         },
         {
             date: '2016-05-04',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1520 弄'
+            address: '上海市普陀区'
         },
         {
             date: '2016-05-02',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
+            address: '呼伦贝尔'
         },
         {
             date: '2016-05-03',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
+            address: '拉萨'
         },
         {
             date: '2016-05-04',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1520 弄'
+            address: '冈仁波齐'
         },
         {
             date: '2016-05-02',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
+            address: '兰州 西宁 青海湖 茶卡盐湖'
         },
         {
             date: '2016-05-03',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
+            address: '泸沽湖 丽江 大理 昆明'
         },
         {
             date: '2016-05-04',
             name: '王小虎',
-            address: '上海市普陀区金沙江路 1520 弄'
-        },
-        {
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-        },
-        {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1520 弄'
-        },
-        {
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-        },
-        {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1520 弄'
+            address: '秦皇岛'
         }
     ]
 }
